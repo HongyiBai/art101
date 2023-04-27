d12ab43cafc15d0d77104c929f9ed44fba0bf233
Hongyi Bai <hobai@ucsc.edu>
2023-04-27 02:36:00 -0700

    finish lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+// Lab 5: Data Types and Variables
+// Define variables in JavaScript
+// Hongyi Bai <hobai@ucsc.edu> Mingze Li <mli168@ucsc.edu>
+// 25 April 2024
+
+//Define Variables
+make = "Mercedes-Benz";
+model = "C300";
+color = "Silver";
+year = 2017;
+
+// calculate
+age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
