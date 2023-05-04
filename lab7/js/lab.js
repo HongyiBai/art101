a7dbf401b84606ef91e4d5771fcebcb3d91b0836
Hongyi Bai <hobai@ucsc.edu>
2023-05-03 21:48:56 -0700

    editing file structure

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -4,22 +4,28 @@
 // 2 May 2023
 
 //sortUserName - a function that takes user input and sorts letters of their name
-function sortUserName(){
-    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
-    console.log("userName =", userName);
+
+
+function sortUserName(name){
+    
+    //console.log("userName =", userName);
     // split string to array
-    var nameArray = userName.split('');
-    console.log("nameArray =", nameArray);
+    //var nameArray = userName.split('');
+    //console.log("nameArray =", nameArray);
     // sort the array
-    var nameArraySort = nameArray.sort();
-    console.log("nameArraySort =", nameArraySort);
+    //var nameArraySort = nameArray.sort();
+    //console.log("nameArraySort =", nameArraySort);
     // join array back to a string
-    var nameSorted = nameArraySort.join('');
-    console.log("nameSorted =", nameSorted);
-    return nameSorted;
+    //var nameSorted = nameArraySort.join('');
+    //console.log("nameSorted =", nameSorted);
+    //return nameSorted;
+
+    return name.split("").sort().join("");
+    
 
 }
-
+var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+var naming = sortUserName(userName);
 // output
 document.writeln("Oh hey, I've fixed your name:",
-    sortUserName(), "</br>");
+    naming, "</br>");
