4fc82449e78f0fc475aa0fed5aba9c5ead0aad3e
Hongyi Bai <hobai@ucsc.edu>
2023-05-03 21:11:44 -0700

    finish lab7

---
+++ lab7/js/lab.js
@@ -0,0 +1,25 @@
+// Lab 7: Functions
+// Write functions that take user input
+// Hongyi Bai <hobai@ucsc.edu> Mingze Li <mli168@ucsc.edu>
+// 2 May 2023
+
+//sortUserName - a function that takes user input and sorts letters of their name
+function sortUserName(){
+    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    console.log("userName =", userName);
+    // split string to array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    // sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    // join array back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    return nameSorted;
+
+}
+
+// output
+document.writeln("Oh hey, I've fixed your name:",
+    sortUserName(), "</br>");
